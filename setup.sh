8335b838394155bb6c1a5539f998c7410796429c
Gabriel Aranda <55933403+gabrielaranda8@users.noreply.github.com>
2020-10-15 20:35:21 -0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,13 +1,9 @@
 mkdir -p ~/.streamlit/
 
 echo "\
-    [general]\n\
-    email = \"gabrielaranda8@gmail.com\"\n\
-    " > ~/.streamlit/credentials.toml
-
-echo "\
-    [server]\n\
-    headless = true\n\
-    enableCORS = false
-    port = $PORT\n\
-    " > ~/.streamlit/config.toml
+[server]\n\
+headless = true\n\
+port = $PORT\n\
+enableCORS = false\n\
+\n\
+" > ~/.streamlit/config.toml
