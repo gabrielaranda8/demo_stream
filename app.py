27a13a64402f82d01bc1630bcdca16e3b1755605
Gabriel Aranda <55933403+gabrielaranda8@users.noreply.github.com>
2020-10-21 23:49:39 -0300

    ahora si en main

--- app.py
+++ app.py
@@ -1,213 +1,184 @@
-# import streamlit as st
 
-# st.title("Mini Tablero")
-# st.subheader("first step")
-# st.text("algo que decir")
+from enum import Enum
+from io import BytesIO, StringIO
+from typing import Union
 
-
-# if __name__ == '__main__':
-#     main()
-# ggfgf
-import streamlit as st
-import altair as alt
-import numpy as np
 import pandas as pd
+from pandas import read_excel
+import streamlit as st
 
+import time
+import sys
 import base64
+import uuid
+import os
+import pickle
+import uuid
+import re
+import time
 
 
-def download_link(object_to_download, download_filename, download_link_text):
+hora = time.strftime("%y%m%d")
+# Uploader widget
+st.sidebar.title("Upload Your File")
+filename = st.sidebar.file_uploader("Choose a file", type=['xlsx', 'csv'])
+st.sidebar.markdown("---")
 
-    if isinstance(object_to_download,pd.DataFrame):
-        object_to_download = object_to_download.to_csv(index=True)
 
+
+
+
+
+def download_button(object_to_download, download_filename, button_text, pickle_it=False):
+    """
+    Generates a link to download the given object_to_download.
+    Params:
+    ------
+    object_to_download:  The object to be downloaded.
+    download_filename (str): filename and extension of file. e.g. mydata.csv,
+    some_txt_output.txt download_link_text (str): Text to display for download
+    link.
+    button_text (str): Text to display on download button (e.g. 'click here to download file')
+    pickle_it (bool): If True, pickle file.
+    Returns:
+    -------
+    (str): the anchor tag to download object_to_download
+    Examples:
+    --------
+    download_link(your_df, 'YOUR_DF.csv', 'Click to download data!')
+    download_link(your_str, 'YOUR_STRING.txt', 'Click to download text!')
+    """
+    if pickle_it:
+        try:
+            object_to_download = pickle.dumps(object_to_download)
+        except pickle.PicklingError as e:
+            st.write(e)
+            return None
+
+    else:
+        if isinstance(object_to_download, bytes):
+            pass
+
+        elif isinstance(object_to_download, pd.DataFrame):
+            object_to_download = object_to_download.to_csv(index=False)
+
+        # Try JSON encode for everything else
+        else:
+            object_to_download = json.dumps(object_to_download)
+
+    try:
+        # some strings <-> bytes conversions necessary here
         b64 = base64.b64encode(object_to_download.encode()).decode()
 
-        return f'<a href="data:file/txt;base64,{b64}" download="{download_filename}">{download_link_text}</a>'
+    except AttributeError as e:
+        b64 = base64.b64encode(object_to_download).decode()
 
-def main():
+    button_uuid = str(uuid.uuid4()).replace('-', '')
+    button_id = re.sub('\d+', '', button_uuid)
 
-    bonos = ["AL29", "AL30","AL35","AE38","AL41","AL29D","AL30D","AL35D","AE38D","AL41D",
-                  "GD29", "GD30", "GD35", "GD38", "GD41", "GD46", "GD29D", "GD30D", "GD35D", "GD38D", "GD41D","GD46D"]
+    custom_css = f""" 
+        <style>
+            #{button_id} {{
+                background-color: rgb(255, 255, 255);
+                color: rgb(38, 39, 48);
+                padding: 0.25em 0.38em;
+                position: relative;
+                text-decoration: none;
+                border-radius: 4px;
+                border-width: 1px;
+                border-style: solid;
+                border-color: rgb(230, 234, 241);
+                border-image: initial;
+            }} 
+            #{button_id}:hover {{
+                border-color: rgb(246, 51, 102);
+                color: rgb(246, 51, 102);
+            }}
+            #{button_id}:active {{
+                box-shadow: none;
+                background-color: rgb(246, 51, 102);
+                color: white;
+                }}
+        </style> """
 
-    periodos = ["Historico", "Intradiario"]
+    dl_link = custom_css + f'<a download="{download_filename}" id="{button_id}" href="data:file/txt;base64,{b64}">{button_text}</a><br></br>'
 
-    st.sidebar.title("Bonos Argy")
-
-    choice = st.sidebar.selectbox("Seleccionar bono 1", bonos)
-    choice1 = st.sidebar.selectbox("Seleccionar bono 2", bonos)
-
-    periodo = st.sidebar.selectbox("Seleccionar periodo", periodos)
-
-    if periodo == 'Historico':
-
-        url = 'http://www.rava.com/empresas/precioshistoricos.php?e=' + choice
-
-        df_1 = pd.read_html(url, thousands='.')[0]
-
-        df_1.columns = list(df_1.loc[0, :])
-        df_1 = df_1.drop(0, axis=0)
-        df_1 = df_1.rename(columns={'Fecha': 'index'}).set_index('index')
-        # df = df.drop(columns=['Apertura', 'Mínimo', 'Volumen', 'Máximo'], axis=1)
-
-        df_1 = df_1.replace('\.', '', regex=True)
-        df_1 = df_1.replace(',', '.', regex=True)
-
-        df_1[df_1.columns[0:5]] = df_1[df_1.columns[0:5]].apply(pd.to_numeric, errors='coerce').round(2)
-
-        df_1.rename(columns=lambda col: choice + '_' + col, inplace=True)
-
-        url = 'http://www.rava.com/empresas/precioshistoricos.php?e=' + choice1
-
-        df_2 = pd.read_html(url, thousands='.')[0]
-
-        df_2.columns = list(df_2.loc[0, :])
-
-        df_2 = df_2.drop(0, axis=0)
-        df_2 = df_2.rename(columns={'Fecha': 'index'}).set_index('index')
-        # df = df.drop(columns=['Apertura', 'Mínimo', 'Volumen', 'Máximo'], axis=1)
-
-        df_2 = df_2.replace('\.', '', regex=True)
-        df_2 = df_2.replace(',', '.', regex=True)
-
-        df_2[df_2.columns[0:5]] = df_2[df_2.columns[0:5]].apply(pd.to_numeric, errors='coerce').round(2)
-
-        if choice == choice1:
-            choice1 = choice1 + '_1'
-
-        df_2.rename(columns=lambda col: choice1 + '_' + col, inplace=True)
-
-        df_ratio = pd.concat([df_1, df_2], axis=1, sort=False)
-
-        # print(df_ratio)
-
-        df_ratio['Ratio'] = df_ratio[choice + '_' + 'Cierre'] / df_ratio[choice1 + '_' + 'Cierre']
-
-        df_ratio.index = pd.to_datetime(df_ratio.index, format='%d/%m/%y')
-
-        df_ratio.sort_index(ascending=True, inplace=True)
-
-        st.dataframe(df_ratio.style.highlight_max(color='green', axis=0).format('{:,.2f}').format({'Ratio': '{:,.5f}'}))
-    # else:
-    #     df = pd.read_csv("intradiario.csv",sep=';')
-    #     df = df.replace('\.', '', regex=True)
-    #     df = df.replace(',', '.', regex=True)
-    #     df = df.rename(columns={'Fecha': 'index'}).set_index('index')
-    #     df [df.columns[0:22]] = df[df.columns[0:22]].apply(pd.to_numeric, errors='coerce').round(2)
-    #     df = df.replace(0,np.nan)
-
-    #     if choice == choice1:
-    #         choice1 = choice1 + '_1'
-    #         df_ratio = df.filter([choice + '_' + 'Cierre'], axis=1)
-    #         df_ratio[choice1 + '_' + 'Cierre'] = df_ratio[choice + '_' + 'Cierre']
-    #     else:
-    #         df_ratio = df.filter([choice + '_' + 'Cierre',choice1 + '_' + 'Cierre'], axis=1)
-
-
-    #     df_ratio['Ratio'] = (df_ratio[choice + '_' + 'Cierre'] / df_ratio[choice1 + '_' + 'Cierre']).replace(np.inf, np.nan )
-
-
-    #     df_ratio.index = pd.to_datetime(df_ratio.index, format='%d/%m/%Y %H:%M')
-
-    #     df_ratio.sort_index(ascending=True, inplace=True)
-
-    #     #cm = sns.light_palette("green", as_cmap=True)
-
-    #     st.dataframe(df_ratio.style.highlight_max(color='green', axis=0).format('{:,.2f}').format({'Ratio':'{:,.5f}'}))
-
-
-    if st.button("Descargar"):
-        # open('bonos.csv', 'w').write(df_ratio.to_csv())
-        tmp_download_link = download_link(df_ratio, 'bonos_' + choice + '_' + choice1 + '.csv',
-                                          'Presione para descargar el archivo')
-        st.markdown(tmp_download_link, unsafe_allow_html=True)
-
-    if st.button("Resumen"):
-        st.write(df_ratio.describe())
-
-    if st.button("Graficar"):
-        cust_data = df_ratio["Ratio"]
-
-        line = alt.Chart(cust_data.reset_index()).mark_line(
-            color='red',
-            size=3
-        ).transform_window(
-            rolling_mean='mean(Ratio)'
-        ).encode(
-            x=alt.X('index'),
-            y=alt.Y('rolling_mean:Q', scale=alt.Scale(zero=False))
-        ).properties(title=choice + '/' + choice1).interactive()
-
-        points = alt.Chart(cust_data.reset_index()).mark_line().encode(
-            x=alt.X('index', axis=alt.Axis(title='Fecha')),
-            y=alt.Y('Ratio:Q',
-                    axis=alt.Axis(title='Ratio - Media'), scale=alt.Scale(zero=False))
-        ).interactive()
-
-        chart = line + points
-
-        st.altair_chart(chart, use_container_width=True)
-
-        base1 = alt.Chart(df_ratio.reset_index()).encode(
-            alt.X('index', axis=alt.Axis(title='Fecha')))
-
-        line_1 = base1.mark_line(
-            color='red',
-            size=3
-        ).encode(
-            y=alt.Y(choice + '_' + 'Cierre:Q', scale=alt.Scale(zero=False),
-                    axis=alt.Axis(title='Cierre', titleColor='red'))
-        ).properties(title=choice).interactive()
-
-        area_1 = base1.mark_area(opacity=0.3, color='#57A44C').encode(
-            alt.Y(choice + '_' + 'Máximo:Q', scale=alt.Scale(zero=False)
-                  ),
-            alt.Y2(choice + '_' + 'Mínimo:Q')
-        )
-
-        bar_1 = base1.mark_bar().encode(
-            alt.Y(choice + '_' + 'Volumen:Q', axis=alt.Axis(title='Volumen', titleColor='red'))
-        )
-
-        layer1 = alt.layer(line_1 + area_1, bar_1).resolve_scale(
-            y='independent'
-        )
-
-        st.altair_chart(layer1, use_container_width=True)
-
-        base2 = alt.Chart(df_ratio.reset_index()).encode(
-            alt.X('index', axis=alt.Axis(title='Fecha')))
-
-        line_2 = base2.mark_line(
-            color='red',
-            size=3
-        ).encode(
-            y=alt.Y(choice1 + '_' + 'Cierre:Q', scale=alt.Scale(zero=False),
-                    axis=alt.Axis(title='Cierre', titleColor='red'))
-        ).properties(title=choice1).interactive()
-
-        area_2 = base2.mark_area(opacity=0.3, color='#57A44C').encode(
-            alt.Y(choice1 + '_' + 'Máximo:Q', scale=alt.Scale(zero=False)
-                  ),
-            alt.Y2(choice1 + '_' + 'Mínimo:Q')
-        )
-
-        bar_2 = base2.mark_bar().encode(
-            alt.Y(choice1 + '_' + 'Volumen:Q', axis=alt.Axis(title='Volumen', titleColor='red')
-                  )
-        )
-
-        layer2 = alt.layer(line_2 + area_2, bar_2).resolve_scale(
-            y='independent'
-        )
-
-        st.altair_chart(layer2, use_container_width=True)
-
-
-    st.sidebar.info('\ntableroooooo')
+    return dl_link
 
 
 
-
+# print('gggggggggggg')
 if __name__ == '__main__':
-    main()
+    if filename:
+        columnas = ['Comitente','CodigoCaja','Cuotas']
+        tablero = pd.read_excel(filename, usecols=columnas)
+        comit = tablero['Comitente']
+        # st.text(comit)
+
+        st.dataframe(tablero)
+        # st.table(tablero)
+    
+     
+
+        datos=open("suscri_tsa1.txt","w")
+        lista_suscri= []
+
+        # -----------------PRIMERAS DOS LINEAS OBLIGATORIAS DEL TXT------------------------------------------
+        linea1 = "00Aftfaot    20"+hora+"1130560000000"
+        lista_suscri.append(linea1)
+
+        incio = "\n"+"0"+hora+"FTFAOT0046"+"\n"
+        lista_suscri.append(incio)
+
+        # -----------------AGREGAMOS LINEAS SEGUN LA CANTIDAD DE SUCRI QUE TENGAMOS-----------------------------------------
+
+        # especie = 5 digitos 
+        # cuotas = 00000000000.0000000  ( 11 y 7) 
+        # comitente = 9 digitos 
+        especie = 0
+        cuotas = 0
+        comitente = 0
+
+        for valor,comit in enumerate(tablero['Comitente']):
+            especie = str(tablero['CodigoCaja'][valor])
+            cuotas = str(tablero['Cuotas'][valor])
+            comitente = str(comit)  
+            
+            if especie!="nan" and cuotas!="nan" and comitente!="nan":
+
+                #### ESPECIE ###############################################
+                especie = str(int(float(especie)))
+                #### COMITENTE #############################################
+                comitente = str(int(float(comitente)))
+                #### CUOTAS ################################################
+                cuotas = str(float(cuotas))
+                
+                ################ AGREGO EL FORMATO A NUESTRO ARCHIVO
+                lista_suscri.append("1'I'E'0046'000000003'"+especie+"       '"+cuotas+"'0046'"+comitente+"'N'00'0000'0000'N"+"\n")
+       
+
+        # LINEA EJEMPLO
+        #"1'I'E'0046'000000003'"+especie+"       '"+cuotas+"'0046'"+comitente+"'N'00'0000'0000'N"
+
+        # ------------------------AGREGAMOS LINEA FINAL---------------------------------------
+
+        # LINEA FINAL
+        num_lineas = len(lista_suscri)-1 # restamos la primera que no cuenta
+        linea_final = "99Aftfaot    20"+hora+"11305600000000"+str(num_lineas)+"\n"
+        lista_suscri.append(linea_final)
+
+        # AGREAGR NUMERO DE FILAS A LA PRIMER LINEA
+        lista_suscri[0] = lista_suscri[0]+str(num_lineas)
+
+
+        datos.writelines(lista_suscri)
+        datos.close() 
+
+        nuevo = "suscri_tsa1.txt"
+        with open(nuevo, 'rb') as f:
+            s = f.read()
+
+        download_button_str = download_button(s, nuevo, f'Click here to download {nuevo}')
+        st.markdown(download_button_str, unsafe_allow_html=True)
+
+        os.remove("suscri_tsa1.txt")
